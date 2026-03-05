2ea6744db916c864629746d51255648e85bf2f81
Anders Jepsen <arjepsen@gmail.com>
2026-03-05 12:53:55 -0600

    Add LVGL+SDL simulator window (sim build)

---
+++ lv_conf.h
@@ -0,0 +1,18 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+/* Basic */
+#define LV_COLOR_DEPTH 16
+#define LV_USE_OS 0
+
+/* Log (optional but useful) */
+#define LV_USE_LOG 1
+
+/* Simulator resolution (close to Brewie UI) */
+#define LV_HOR_RES_MAX 480
+#define LV_VER_RES_MAX 272
+
+/* Enable SDL driver in LVGL */
+#define LV_USE_SDL 1
+
+#endif /*LV_CONF_H*/
--- src/main.c
+++ src/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
+#include "lvgl.h"
 
-int main()
+/* LVGL v9 SDL driver headers */
+#include "src/drivers/sdl/lv_sdl_window.h"
+#include "src/drivers/sdl/lv_sdl_mouse.h"
+#include "src/drivers/sdl/lv_sdl_mousewheel.h"
+#include "src/drivers/sdl/lv_sdl_keyboard.h"
+
+static void log_cb(lv_log_level_t level, const char * buf)
 {
-    puts("brewie_ui_rebuild starting");
+    (void)level;
+    fputs(buf, stderr);
+    fputc('\n', stderr);
+}
+
+int main(void)
+{
+    lv_init();
+
+#if LV_USE_LOG
+    lv_log_register_print_cb(log_cb);
+#endif
+
+    /* Create SDL window + input devices */
+    lv_display_t * disp = lv_sdl_window_create(480, 272);
+    (void)disp;
+    lv_sdl_mouse_create();
+    lv_sdl_mousewheel_create();
+    lv_sdl_keyboard_create();
+
+    /* Simple UI sanity check */
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "LVGL sim is running");
+    lv_obj_center(label);
+
+    while(1) {
+        lv_timer_handler();
+        lv_tick_inc(5);
+        usleep(5000);
+    }
+
     return 0;
 }
